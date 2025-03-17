0df57775cec3f8af9710c4c4a271078f5466f0fe
Valgrm <166134642+Valgrm@users.noreply.github.com>
2025-03-17 17:58:36 +0100

    Page profil

--- front/src/views/Profile/ProfileView.vue
+++ front/src/views/Profile/ProfileView.vue
@@ -131,5 +131,32 @@ const CouleurRoles = (roleLabel) => {
                 </div>
             </div>
         </div>
+      <div class="mx-auto max-w-screen-xl px-4 sm:px-6 lg:px-8">
+        <div class="mt-8 md:flex md:justify-between block w-full gap-4">
+          <div class="w-full">
+            <div class="px-4 py-5 sm:px-6 items-center">
+              <div>
+                <h3 class="text-lg font-medium leading-6 text-gray-900">Devoirs ajoutés</h3>
+                <p class="mt-1 max-w-2xl text-sm text-gray-500">Gérez les devoirs ajoutés</p>
+              </div>
+            </div>
+          </div>
+          <div class="w-full">
+            <form class="border border-gray-200 bg-white shadow-sm sm:rounded-lg">
+              <div class="w-full">
+                <div class="sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6 sm:py-5">
+                  <div class="text-sm font-normal text-gray-500">Intitulé</div>
+                  <div class="mt-1 text-sm text-gray-900 sm:col-span-2">{{user.id_classes.intitule}}</div>
+                </div>
+                <div class="border-t border-gray-200"></div>
+                <div class="sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6 sm:py-5">
+                  <div class="text-sm font-normal text-gray-500">Promo</div>
+                  <div class="mt-1 text-sm text-gray-900 sm:col-span-2">{{user.id_classes.promo}}</div>
+                </div>
+              </div>
+            </form>
+          </div>
+        </div>
+      </div>
     </section>
 </template>
