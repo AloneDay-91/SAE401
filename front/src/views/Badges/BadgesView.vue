9adb7fddc4267f36b50359f5f301d0e378162bb2
Valgrm <166134642+Valgrm@users.noreply.github.com>
2025-03-21 11:58:24 +0100

    Ajout des badges

--- front/src/views/Badges/BadgesView.vue
+++ front/src/views/Badges/BadgesView.vue
@@ -8,7 +8,98 @@ const API_URL = import.meta.env.VITE_API_BASE_URL;
 const store = useStore();
 const user = ref(store.state.user);
 const token = localStorage.getItem('token');
+
 const devoirsAjoutes = ref(0);
+const devoirsTermines = ref(12);
+const devoirsAvance = ref(8);
+const devoirsVotes = ref(5);
+
+// Les niveaux pour chaque catégorie
+const niveauxAjoutes = [
+  { niveau: 1, objectif: 0, badge: "Ajouter_Lvl_1.svg" },
+  { niveau: 2, objectif: 6, badge: "Ajouter_Lvl_2.svg" },
+  { niveau: 3, objectif: 15, badge: "Ajouter_Lvl_3.svg" },
+];
+
+const niveauxTermines = [
+  { niveau: 1, objectif: 0, badge: "Terminer_Lvl_1.svg" },
+  { niveau: 2, objectif: 5, badge: "Terminer_Lvl_2.svg" },
+  { niveau: 3, objectif: 12, badge: "Terminer_Lvl_3.svg" },
+];
+
+const niveauxAvances = [
+  { niveau: 1, objectif: 0, badge: "Avance_Lvl_1.svg" },
+  { niveau: 2, objectif: 8, badge: "Avance_Lvl_2.svg" },
+  { niveau: 3, objectif: 20, badge: "Avance_Lvl_3.svg" },
+];
+
+const niveauxVotes = [
+  { niveau: 1, objectif: 0, badge: "Voter_Lvl_1.svg" },
+  { niveau: 2, objectif: 10, badge: "Voter_Lvl_2.svg" },
+  { niveau: 3, objectif: 25, badge: "Voter_Lvl_3.svg" },
+];
+
+// Calculer le niveau actuel pour chaque catégorie
+const niveauAjoutes = computed(() => {
+  for (let i = niveauxAjoutes.length - 1; i >= 0; i--) {
+    if (devoirsAjoutes.value >= niveauxAjoutes[i].objectif) return niveauxAjoutes[i];
+  }
+  return niveauxAjoutes[0];
+});
+
+const niveauTermines = computed(() => {
+  for (let i = niveauxTermines.length - 1; i >= 0; i--) {
+    if (devoirsTermines.value >= niveauxTermines[i].objectif) return niveauxTermines[i];
+  }
+  return niveauxTermines[0];
+});
+
+const niveauAvances = computed(() => {
+  for (let i = niveauxAvances.length - 1; i >= 0; i--) {
+    if (devoirsAvance.value >= niveauxAvances[i].objectif) return niveauxAvances[i];
+  }
+  return niveauxAvances[0];
+});
+
+const niveauVotes = computed(() => {
+  for (let i = niveauxVotes.length - 1; i >= 0; i--) {
+    if (devoirsVotes.value >= niveauxVotes[i].objectif) return niveauxVotes[i];
+  }
+  return niveauxVotes[0];
+});
+
+// Calculer les objectifs des prochains niveaux
+const prochainObjectifAjoutes = computed(() => {
+  const index = niveauxAjoutes.findIndex(n => n.niveau === niveauAjoutes.value.niveau);
+  return index < niveauxAjoutes.length - 1 ? niveauxAjoutes[index + 1].objectif : niveauAjoutes.value.objectif;
+});
+
+const prochainObjectifTermines = computed(() => {
+  const index = niveauxTermines.findIndex(n => n.niveau === niveauTermines.value.niveau);
+  return index < niveauxTermines.length - 1 ? niveauxTermines[index + 1].objectif : niveauTermines.value.objectif;
+});
+
+const prochainObjectifAvances = computed(() => {
+  const index = niveauxAvances.findIndex(n => n.niveau === niveauAvances.value.niveau);
+  return index < niveauxAvances.length - 1 ? niveauxAvances[index + 1].objectif : niveauAvances.value.objectif;
+});
+
+const prochainObjectifVotes = computed(() => {
+  const index = niveauxVotes.findIndex(n => n.niveau === niveauVotes.value.niveau);
+  return index < niveauxVotes.length - 1 ? niveauxVotes[index + 1].objectif : niveauVotes.value.objectif;
+});
+
+// Calculer les pourcentages de progression
+const pourcentageAjoutes = computed(() => Math.min((devoirsAjoutes.value / prochainObjectifAjoutes.value) * 100, 100));
+const pourcentageTermines = computed(() => Math.min((devoirsTermines.value / prochainObjectifTermines.value) * 100, 100));
+const pourcentageAvances = computed(() => Math.min((devoirsAvance.value / prochainObjectifAvances.value) * 100, 100));
+const pourcentageVotes = computed(() => Math.min((devoirsVotes.value / prochainObjectifVotes.value) * 100, 100));
+
+// Badges actuels
+const badgeAjoutes = computed(() => `/src/assets/badges/${niveauAjoutes.value.badge}`);
+const badgeTermines = computed(() => `/src/assets/badges/${niveauTermines.value.badge}`);
+const badgeAvances = computed(() => `/src/assets/badges/${niveauAvances.value.badge}`);
+const badgeVotes = computed(() => `/src/assets/badges/${niveauVotes.value.badge}`);
 
 // Fonction pour récupérer le nombre de devoirs ajoutés
 const fetchDevoirsAjoutes = async () => {
@@ -36,45 +127,8 @@ onMounted(() => {
     console.error("Utilisateur non connecté ou ID manquant");
   }
 });
-
-// Définir les niveaux et leurs objectifs
-const niveaux = [
-  { niveau: 1, objectif: 10, badge: "Ajouter_Lvl_1.svg" },
-  { niveau: 2, objectif: 20, badge: "Ajouter_Lvl_2.svg" },
-  { niveau: 3, objectif: 30, badge: "Ajouter_Lvl_3.svg" },
-];
-
-// Calculer le niveau actuel en fonction du nombre de devoirs ajoutés
-const niveauActuel = computed(() => {
-  for (let i = niveaux.length - 1; i >= 0; i--) {
-    if (devoirsAjoutes.value >= niveaux[i].objectif) {
-      return niveaux[i];
-    }
-  }
-  return niveaux[0];
-});
-
-// Calculer l'objectif du prochain niveau
-const prochainObjectif = computed(() => {
-  const indexNiveauActuel = niveaux.findIndex(n => n.niveau === niveauActuel.value.niveau);
-  if (indexNiveauActuel < niveaux.length - 1) {
-    return niveaux[indexNiveauActuel + 1].objectif;
-  }
-  return niveauActuel.value.objectif;
-});
-
-// Calculer le pourcentage de la barre de progression
-const pourcentageProgression = computed(() => {
-  const objectif = prochainObjectif.value;
-  return Math.min((devoirsAjoutes.value / objectif) * 100, 100);
-});
-
-// Badge à afficher
-const badgeActuel = computed(() => {
-  return `/src/assets/badges/${niveauActuel.value.badge}`;
-});
-
 </script>
+
 <template>
   <section class="min-h-screen pb-8 border border-l-0 border-r-0 border-gray-200">
     <div class="mx-auto max-w-screen-xl px-4 py-8 sm:px-6 sm:py-12 lg:px-8">
@@ -89,51 +143,75 @@ const badgeActuel = computed(() => {
         </div>
       </div>
       <div class="mt-8">
+        <!-- Devoirs Ajoutés -->
         <div class="bg-white shadow-sm sm:rounded-lg border border-gray-200 mt-6">
           <div class="px-4 py-5 sm:px-6 md:flex md:items-center md:justify-start gap-4 block">
             <div class="max-w-md">
-              <img :src="badgeActuel" alt="Badge Actuel">
+              <img :src="badgeAjoutes" alt="Badge Ajoutés">
             </div>
             <div class="w-full">
               <h3 class="text-lg font-medium leading-6 text-gray-900">Devoirs Ajoutés</h3>
-              <p class="mt-1 max-w-2xl text-sm text-gray-500">
-                Ajouter {{ prochainObjectif }} devoirs
-              </p>
               <div class="flex items-center gap-2 text-xs font-light">
                 <div class="mt-2 w-full h-2 bg-gray-200 rounded-full">
-                  <div
-                      :style="{ width: pourcentageProgression + '%' }"
-                      class="h-full bg-green-400 rounded-full"
-                  ></div>
+                  <div :style="{ width: pourcentageAjoutes + '%' }" class="h-full bg-green-400 rounded-full transition-all duration-300 ease-in-out"></div>
                 </div>
-                <div>
-                  <span>{{ devoirsAjoutes }}/{{ prochainObjectif }}</span>
-                </div>
+                <div><span>{{ devoirsAjoutes }}/{{ prochainObjectifAjoutes }}</span></div>
               </div>
+              <p class="mt-1 max-w-2xl text-sm text-gray-500">Ajouter {{ prochainObjectifAjoutes }} devoirs</p>
             </div>
           </div>
         </div>
+        <!-- Devoirs Terminés -->
         <div class="bg-white shadow-sm sm:rounded-lg border border-gray-200 mt-6">
           <div class="px-4 py-5 sm:px-6 md:flex md:items-center md:justify-start gap-4 block">
             <div class="max-w-md">
-              <img :src="badgeActuel" alt="Badge Actuel">
+              <img :src="badgeTermines" alt="Badge Terminés">
             </div>
             <div class="w-full">
-              <h3 class="text-lg font-medium leading-6 text-gray-900">Devoirs Ajoutés</h3>
-              <p class="mt-1 max-w-2xl text-sm text-gray-500">
-                Ajouter {{ prochainObjectif }} devoirs
-              </p>
+              <h3 class="text-lg font-medium leading-6 text-gray-900">Devoirs Terminés</h3>
               <div class="flex items-center gap-2 text-xs font-light">
                 <div class="mt-2 w-full h-2 bg-gray-200 rounded-full">
-                  <div
-                      :style="{ width: pourcentageProgression + '%' }"
-                      class="h-full bg-green-400 rounded-full"
-                  ></div>
+                  <div :style="{ width: pourcentageTermines + '%' }" class="h-full bg-green-400 rounded-full transition-all duration-300 ease-in-out"></div>
                 </div>
-                <div>
-                  <span>{{ devoirsAjoutes }}/{{ prochainObjectif }}</span>
-                </div>
+                <div><span>{{ devoirsTermines }}/{{ prochainObjectifTermines }}</span></div>
               </div>
+              <p class="mt-1 max-w-2xl text-sm text-gray-500">Terminer {{ prochainObjectifTermines }} devoirs</p>
+            </div>
+          </div>
+        </div>
+        <!-- Devoirs Avancés -->
+        <div class="bg-white shadow-sm sm:rounded-lg border border-gray-200 mt-6">
+          <div class="px-4 py-5 sm:px-6 md:flex md:items-center md:justify-start gap-4 block">
+            <div class="max-w-md">
+              <img :src="badgeAvances" alt="Badge Avancés">
+            </div>
+            <div class="w-full">
+              <h3 class="text-lg font-medium leading-6 text-gray-900">Devoirs Avancés</h3>
+              <div class="flex items-center gap-2 text-xs font-light">
+                <div class="mt-2 w-full h-2 bg-gray-200 rounded-full">
+                  <div :style="{ width: pourcentageAvances + '%' }" class="h-full bg-green-400 rounded-full transition-all duration-300 ease-in-out"></div>
+                </div>
+                <div><span>{{ devoirsAvance }}/{{ prochainObjectifAvances }}</span></div>
+              </div>
+              <p class="mt-1 max-w-2xl text-sm text-gray-500">Avancer {{ prochainObjectifAvances }} devoirs</p>
+            </div>
+          </div>
+        </div>
+        <!-- Devoirs Votés -->
+        <div class="bg-white shadow-sm sm:rounded-lg border border-gray-200 mt-6">
+          <div class="px-4 py-5 sm:px-6 md:flex md:items-center md:justify-start gap-4 block">
+            <div class="max-w-md">
+              <img :src="badgeVotes" alt="Badge Votés">
+            </div>
+            <div class="w-full">
+              <h3 class="text-lg font-medium leading-6 text-gray-900">Devoirs Votés</h3>
+              <div class="flex items-center gap-2 text-xs font-light">
+                <div class="mt-2 w-full h-2 bg-gray-200 rounded-full">
+                  <div :style="{ width: pourcentageVotes + '%' }" class="h-full bg-green-400 rounded-full transition-all duration-300 ease-in-out"></div>
+                </div>
+                <div><span>{{ devoirsVotes }}/{{ prochainObjectifVotes }}</span></div>
+              </div>
+              <p class="mt-1 max-w-2xl text-sm text-gray-500">Voter pour {{ prochainObjectifVotes }} devoirs</p>
             </div>
           </div>
         </div>
